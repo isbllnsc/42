9b3f4cef9c26f82163c2c7b12bcbc17b95944a53
Isabella de Souza Nascimento <isabellanascimento@MacBook-Air-de-Isabella.local>
2025-11-13 23:44:36 -0300

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -16,34 +16,61 @@
 char	**ft_split(char const *s, char c)
 {
 	int	i;
+	int	w;
 	int	countw;
 	int	start;
 	int	lenw;
 	char    **list;
+	int	j;
 
 	i = 0;
+	w = 0;
 	countw = 0;
 	lenw = 0;
-	while (s[i])
+	while (s[i]) // to count words
 	{
-		while (s[i] && (s[i] <= 32 || s[i] == c))
+		while (s[i] && (s[i] < 33 || s[i] == c))
 			i++;
 		
 		countw++;
 
-		start = i;
-		while (s[i] && (s[i] != c && s[i] > 32))
+		while (s[i] && (s[i] > 32 && s[i] != c))
 		{
 			i++;
 		}
-		lenw = i - start;
-		list = malloc(sizeof(char *) * (countw + 1)); //pointers array
-		list[i] = malloc(sizeof(char) * (lenw + 1));
 	}
 
-	list[countw] = NULL;
+	list = malloc(sizeof(char *) * (countw + 1)); // to allocate array of strs
 
-	return (list);
+	i = 0;
+	while (s[i]) // to find the lenght of the words(strings)
+	{
+		while (s[i] && (s[i] < 33 || s[i] == c))
+			i++;
+
+		start = i;
+		
+		while (s[i] && (s[i] > 32 && s[i] != c))
+			i++;
+
+		lenw = i - start; // start = beginning of each string; i = ending
+
+		list[w] = malloc(sizeof(char) * (lenw + 1)); // to allocate strs
+
+
+		j = 0;
+		while (j < lenw)
+		{
+			list[w][j] = s[start + j];
+			j++;
+		}
+		list[w][j] = '\0';
+		w++;
+	}
+
+		list[w] = NULL;
+
+		return (list);
 }
 
 int	main()
