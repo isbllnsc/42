db0a79b674046f06595e875e43c9be5ff19dfe88
Isabella de Souza Nascimento <isabellanascimento@MacBook-Air-de-Isabella.local>
2025-12-07 22:19:38 -0300

    final

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -12,25 +12,25 @@
 
 #include "ft_printf.h"
 
-static int	print_type(char type, va_list *args)
+static int	print_type(char type, va_list args)
 {
 	int	count;
 
 	count = 0;
 	if (type == 'u')
-		count += ft_printf_u(va_arg(*args, unsigned int));
+		count += ft_printf_u(va_arg(args, unsigned int));
 	else if (type == 'c')
-		count += ft_printf_chr(va_arg(*args, int));
+		count += ft_printf_chr(va_arg(args, int));
 	else if (type == 's')
-		count += ft_printf_str(va_arg(*args, char *));
+		count += ft_printf_str(va_arg(args, char *));
 	else if (type == 'x')
-		count += ft_printf_x_lower(va_arg(*args, unsigned int));
+		count += ft_printf_x_lower(va_arg(args, unsigned int));
 	else if (type == 'X')
-		count += ft_printf_x_upper(va_arg(*args, unsigned int));
+		count += ft_printf_x_upper(va_arg(args, unsigned int));
 	else if (type == 'p')
-		count += ft_printf_p(va_arg(*args, void *));
+		count += ft_printf_p(va_arg(args, void *));
 	else if (type == 'd' || type == 'i')
-		count += ft_printf_id(va_arg(*args, int));
+		count += ft_printf_id(va_arg(args, int));
 	else if (type == '%')
 		write(1, "%", 1);
 	else
@@ -54,7 +54,7 @@ int	ft_printf(const char *str, ...)
 		if (str[i] == '%')
 		{
 			i++;
-			count += print_type(str[i], &args);
+			count += print_type(str[i], args);
 		}
 		else
 			count += write(1, &str[i], 1);
@@ -63,7 +63,7 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (count);
 }
-/*
+
 int	main()
 {
 	char chr = 'a';
@@ -74,4 +74,4 @@ int	main()
 	ft_printf("hex a: %x; hex  M: %X\n", chr, chr2);
 	ft_printf("p: %p\n", str);
 	ft_printf("i: %i e d: %d\n", 42, 42);
-}*/
+}
