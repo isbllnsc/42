7c3f456ae9c9b6dfb79e5491cff1cc1b653ebcfa
Isabella de Souza Nascimento <isabellanascimento@MacBook-Air-de-Isabella.local>
2025-12-16 22:14:12 -0300

    2

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,7 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// assinatura de read = ssize_t read(int fd, void *buf, size_t count);
+
 char	*get_next_line(int fd)
 {
-	
+    static char	*stash[OPEN_MAX];
+	char	*line;
+    char	buffer[BUFFER_SIZE + 1];
+	ssize_t	bytes_read;
+
+	bytes_read = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (!check_newline(stash[fd]))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (NULL);
+		if (bytes_read <= 0)
+			break;
+		buffer[bytes_read] = '\0';
+		stash[fd] = ft_strjoin(stash[fd], buffer);
 }
+
+		if (!stash[fd] || stash[fd][0] == '\0')
+		{
+			free(stash[fd]);
+			stash[fd] = NULL;
+			return (NULL);
+		}
